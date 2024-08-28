dbcf638bd27c5af56b94fbb2982d3da95e680948
ismail elmtioui <ismailelmtioui2357@gmail.com>
2024-08-28 22:38:32 +0200

    videos/forgetpass

---
+++ src/main/java/com/_DSF/je/Controller/ForgotPasswordController.java
@@ -0,0 +1,40 @@
+package com._DSF.je.Controller;
+
+import com._DSF.je.Entity.User;
+import com._DSF.je.Repository.UserRepository;
+import com._DSF.je.Service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/auth")
+public class ForgotPasswordController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/forgot-password")
+    public ResponseEntity<String> forgotPassword(@RequestParam String email) {
+        // Find user by email
+        Optional<User> userOptional = userRepository.findByEmail(email);
+
+        if (userOptional.isPresent()) {
+            // Get user's password
+            String password = userOptional.get().getPassword();
+
+            // Send password via email
+            emailService.sendEmail(email, "Your Password", "Your password is: " + password);
+
+            return ResponseEntity.ok("Password has been sent to your email.");
+        } else {
+            return ResponseEntity.status(404).body("Email not found.");
+        }
+    }
+}
